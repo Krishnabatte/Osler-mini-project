c1d8be75a877afeeefc9da2a47beacfa6821f25f
Krishnabatte <chandu.batte@gmail.com>
2024-10-30 11:10:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "jenkins_server" {
   }
 }
 
-output "jenkins_url" {
-  value = "http://${aws_instance.jenkins_server.public_ip}:8080"
-  description = "Jenkins Server URL"
-}
+#output "jenkins_url" {
+ # value = "http://${aws_instance.jenkins_server.public_ip}:8080"
+ # description = "Jenkins Server URL"
+#}

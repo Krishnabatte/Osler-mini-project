0c3e66850a8260888c1fe72fcb231beb1954422e
Krishnabatte <chandu.batte@gmail.com>
2024-10-30 13:16:22 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,9 +6,9 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "key_name" {
-  description = "SSH_key"
-}
+#variable "key_name" {
+#  description = "SSH_key"
+#}
 
 #variable "vpc_security_group_ids" {
 #  description = "List of security group IDs for the instance"
